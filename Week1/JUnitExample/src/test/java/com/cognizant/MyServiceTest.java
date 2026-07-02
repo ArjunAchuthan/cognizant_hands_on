943903f1584b7991cd2a7c9f709678620edd7fb3
ArjunAchuthan <arjun.achuthan05@gmail.com>
2026-07-03 00:34:54 +0530

    Week1 Verifying Interactions

--- Week1/JUnitExample/src/test/java/com/cognizant/MyServiceTest.java
+++ Week1/JUnitExample/src/test/java/com/cognizant/MyServiceTest.java
@@ -10,13 +10,10 @@ public class MyServiceTest {
     @Test
     public void testExternalApi() {
 
-        // Step 1: Create Mock Object
         ExternalApi mockApi = mock(ExternalApi.class);
 
-        // Step 2: Stub Method
         when(mockApi.getData()).thenReturn("Mock Data");
 
-        // Step 3: Use Mock Object
         MyService service = new MyService(mockApi);
 
         String result = service.fetchData();
