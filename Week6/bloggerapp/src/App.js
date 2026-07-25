9054c8d7c635c87739de58accfe312ddcc6573c8
ArjunAchuthan <arjun.achuthan05@gmail.com>
2026-07-26 01:22:37 +0530

    13. ReactJS-HOL

---
+++ Week6/bloggerapp/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+import BookDetails from "./BookDetails";
+import BlogDetails from "./BlogDetails";
+import CourseDetails from "./CourseDetails";
+
+function App() {
+
+  return (
+
+    <div className="container">
+
+      <CourseDetails />
+
+      <BookDetails />
+
+      <BlogDetails />
+
+    </div>
+
+  );
+
+}
+
+export default App;
